38df68974e07eb9c2fbc0719fbf2e931360045cf
Igor Data <igor.s.data@gmail.com>
2018-08-21 22:09:18 +0300

    updated example

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import Link from 'next/link'
 export default () => (
     <div>
-        <Link href="#some"><a>that one doesn't work</a></Link>        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        <a href="#some">this one works</a>                            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        <a name="some"/>#some                                         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <Link href="#some"><a>that one doesn't work</a></Link><br/>
+        <a href="#some">this one works</a>
+
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+        <a name="some"/>#some
+
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
     </div>
 )
