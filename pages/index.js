812ddaf03bf658513301671d7b764ae48e7b3659
Igor Data <igor.s.data@gmail.com>
2018-08-21 21:59:21 +0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Link from 'next/link'
+export default () => (
+    <div>
+        <Link href="#some"><a>that one doesn't work</a></Link>        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <a href="#some">this one works</a>                            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <a name="some"/>#some                                         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+    </div>
+)
